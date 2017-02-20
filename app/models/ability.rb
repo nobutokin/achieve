cf78f8df1a05cf8d692c24bb82a42575d6f28263
Nobuto Kinjyo <nobuto0502@gmail.com>
2017-02-20 08:50:36 +0000

    メール機能追加

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user && user.admin?
-      can :access, :rails_admin   # grant access to rails_admin
-      can :manage, :all           # allow superadmins to do anything
-    end
+    # if user && user.admin?
+    #   can :access, :rails_admin   # grant access to rails_admin
+    #   can :manage, :all           # allow superadmins to do anything
+    # end
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214174459) do
+ActiveRecord::Schema.define(version: 20170217161121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,8 +55,13 @@ ActiveRecord::Schema.define(version: 20170214174459) do
     t.datetime "updated_at",                             null: false
     t.string   "name"
     t.boolean  "admin",                  default: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
