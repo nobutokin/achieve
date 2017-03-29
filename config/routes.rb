91c0938ffc643c3c5cc23d87669259f5790b5b48
Nobuto <nobuto0502@gmail.com>
2017-03-29 12:22:50 +0000

    DIVE19_1

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,10 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :conversations do
+    resources :messages
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
